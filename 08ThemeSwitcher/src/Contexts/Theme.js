8e375848b1458dba3a262d9e1832c75157a19c8e
Shovan <mondalshovan28701@gmail.com>
2025-06-13 15:16:07 +0530

    added contextAPI2

---
+++ 08ThemeSwitcher/src/Contexts/Theme.js
@@ -0,0 +1,14 @@
+import { createContext, useContext } from "react";
+
+export const ThemeContext = createContext({
+    themeMode: "light", // Default theme mode
+    darkTheme: () => {}, // Function to set dark theme
+    lightTheme: () => {}, // Function to set light theme
+});
+
+export const ThemeProvider = ThemeContext.Provider;
+
+export default function useTheme() {
+    return useContext(ThemeContext);
+}
+
---
+++ 08ThemeSwitcher/src/Contexts/UserContext.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const UserContext = React.createContext();
+
+export default UserContext;
+// This code creates a UserContext using React's Context API.
